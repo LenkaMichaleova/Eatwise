593ba84c2c1aed1dfc48491fcfa10f767640b24b
Lenka Michaleová <michaleova.lenka@gmail.com>
2025-11-10 08:22:01 +0100

    added react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import { BrowserRouter, Routes, Route } from 'react-router';
+
 function App() {
   return (
-    <>
-      <h1>Eatwise</h1>
-    </>
+    <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<h1>Eatwise</h1>} />
+        </Routes>
+    </BrowserRouter>
   );
 }
 
