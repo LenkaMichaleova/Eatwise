de5e54dc9e67336ba0c08f9c1186106f40f2fc31
Lenka Michaleová <michaleova.lenka@gmail.com>
2025-11-13 09:09:29 +0100

    eslint and prettier fix

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,9 @@
 export const ROUTES = {
-    home: '/', 
-    profile: '/profile', 
-    database: '/database', 
-    menu: '/menu', 
-    tables: '/tables', 
-    countCalories: '/count-calories',
-    error: '*', 
-}
+  home: '/',
+  profile: '/profile',
+  database: '/database',
+  menu: '/menu',
+  tables: '/tables',
+  countCalories: '/count-calories',
+  error: '*',
+};
