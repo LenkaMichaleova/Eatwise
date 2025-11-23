04acc5ef2c724a122f868bf779c5c6af59f3429e
Lenka Michaleová <michaleova.lenka@gmail.com>
2025-11-23 11:54:21 +0100

    Add SectionTitle component for displaying titles with a back navigation button

---
+++ src/components/SectionTitle.tsx
@@ -0,0 +1,21 @@
+import { Box, Button, Typography } from '@mui/material';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import { Link } from 'react-router-dom';
+import { ROUTES } from '../routes';
+
+interface SectionTitleProps {
+  title: string;
+}
+
+export const SectionTitle = ({ title }: SectionTitleProps) => {
+  return (
+    <Box sx={{ display: 'flex', alignItems: 'center' }}>
+      <Button component={Link} to={ROUTES.home}>
+        <ArrowBackIcon fontSize="large" />
+      </Button>
+      <Typography variant="h2" color="primary">
+        {title}
+      </Typography>
+    </Box>
+  );
+};
