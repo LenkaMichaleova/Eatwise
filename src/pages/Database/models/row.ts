9cb1dffe675222014de973a5a2facb13beeba817
Lenka Michaleová <michaleova.lenka@gmail.com>
2025-11-23 14:31:39 +0100

    Fix formatting of type declaration in Row model

--- src/pages/Database/models/row.ts
+++ src/pages/Database/models/row.ts
@@ -1,7 +1,7 @@
 export type Row = {
   id: number;
   name: string;
-  type: 'snídaně' | 'svačina1' | 'oběd' |'svačina2' | 'večeře';
+  type: 'snídaně' | 'svačina1' | 'oběd' | 'svačina2' | 'večeře';
   calories: number;
   fat: number;
   carbs: number;
