f91298dab374a3ced25b63782c4882427eca5625
Lenka Michaleová <michaleova.lenka@gmail.com>
2026-02-26 13:49:38 +0100

    fix: update typography variant in DatabaseMealCard and adjust ingredient card height for responsiveness

--- src/pages/Database/components/DatabaseMealCard.tsx
+++ src/pages/Database/components/DatabaseMealCard.tsx
@@ -40,7 +40,7 @@ export const DatabaseMealCard = ({ data }: DatabaseMealCardProps) => {
           title={
             <Box>
               <Typography
-                variant="subtitle1"
+                variant="body1"
                 color="primary"
                 width="80%"
                 gutterBottom
